0a39fe1208aa67f02b33b0a7f678c84db73792d1
Faisal ahmed <faisal2410@yahoo.com>
2023-09-21 11:58:52 +0600

    updated

---
+++ note.js
@@ -0,0 +1,19 @@
+// nested function
+// closure
+// callback function
+// higher order function
+// pur function impure function
+// IIFE->Immediately Invoked Function Expression
+// ()-> group operator
+/*
+ (function () {
+    
+ })() ->iife
+
+
+*/ 
+
+// call stack
+// function execution
+// Recursion
+
