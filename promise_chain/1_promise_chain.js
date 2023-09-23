ed7f3568ea92a91d9f5c24060ef7ccc962e9a7d2
Faisal ahmed <faisal2410@yahoo.com>
2023-09-23 09:49:16 +0600

    promise chain explained

---
+++ promise_chain/1_promise_chain.js
@@ -0,0 +1,31 @@
+// Chaining Promises - Basic Usage
+
+//✅Example1 Handling resolve with the .then() handler
+// Create a Promise
+// let promise = new Promise(function(resolve, reject) {
+//     resolve('Resolving a fake Promise.');
+// });
+
+// // Handle it using the .then() handler
+// promise.then(function(value) {
+//     console.log(value);
+// });
+
+
+
+// ✅Example2
+
+// Create a Promise
+let promise = new Promise(function (resolve, reject) {
+    resolve("Resolving a fake Promise.");
+  });
+  
+  // Create a Promise Chain
+  promise
+    .then(function (value) {
+      console.log(value);
+      return value;
+    })
+    .then(function (value) {
+      console.log(`${value} Second time.`);
+    });
