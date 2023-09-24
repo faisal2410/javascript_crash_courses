8549e45c3214fb9e15686ca6bc1fa419dc39e80d
Faisal ahmed <faisal2410@yahoo.com>
2023-09-24 20:29:10 +0600

    object destructuring explained

---
+++ 12_object_destructuring/5_with_function.js
@@ -0,0 +1,21 @@
+const employee = {
+    id: 7,
+    name: 'James',
+    dept: 'Spy'
+  }
+  
+  function logEmployee({name, dept}) {
+    console.log(`${name} is ${dept}`);
+  }
+  
+  logEmployee(employee);
+  
+  
+  function getUser() {
+    return {
+      'name': 'Alex',
+      'age': 45
+    }
+  }
+  
+  const { age } = getUser();
